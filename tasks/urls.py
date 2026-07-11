5c1c02bc6855c87e3a0f33b04f790a43d540e705
Robinson <auxsenadesarrollo@consuerte.com.co>
2026-07-11 11:57:48 -0500

    2026/07/11 Creacion de endpoints y mejoras en CSS

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.tasks, name = 'tasks'),
     path('create/', views.create_task, name= 'create'),
-    path('edit/', views.edit_task, name= 'edit')
+    path('<int:id>', views.details_task, name= 'detail'),
+    path('<int:id>/edit/', views.edit_task, name= 'edit'),
+    # path('<int:id>/edit', views.details_task, name= 'edit'),
+    # path('<int:id/delete>', views.details_task, name= 'delete')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def tasks(request):
 
     task = Task.objects.all()
-
     return render(request, 'tasks/tasks.html',{
         'tasks': task
     })
@@ -14,9 +13,10 @@ def create_task(request):
         return render(request, 'tasks/create_task.html')
     
     titulo = request.POST['title']
-    descripcion = request.POST['description']        # prioridad = request.POST['important']
+    descripcion = request.POST['description'] 
+    prioridad = 'important' in request.POST
 
-    tarea = Task(title = titulo, description = descripcion)
+    tarea = Task(title = titulo, description = descripcion, important = prioridad)
     tarea.save()
 
     return redirect('tasks')
@@ -25,5 +25,24 @@ def create_task(request):
 def edit_task(request):
     if request.method == 'GET':
         return render(request, 'tasks/edit_task.html')
+
+
+def details_task(request, id):
+
+    task = get_object_or_404(Task, id = id)
+    return render(request, 'tasks/details_task.html', {
+        'task': task
+    })
+
+
+def edit_task(request, id):
+    if request.method == "GET":
+        return render(request, 'tasks/edit_task.html')
+
+    # task = Task.objects.get(id = id)
+
+
+
+
 
 
